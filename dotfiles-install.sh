505a11f5a4b1162c15a0159ab8a7011cc966e49c
StringRam <27649122+StringRam@users.noreply.github.com>
2025-11-25 16:13:24 -0300

    Remove Hyprland QT utils check from configuration

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -240,7 +240,6 @@ exec-once = regreet; hyprctl dispatch exit
 misc {
     disable_hyprland_logo = true
     disable_splash_rendering = true
-    disable_hyprland_qtutils_check = true
 }
 EOF
     sudo systemctl enable greetd.service
